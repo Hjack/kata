b87f875e9af68070e200b7063b26a49ccc4e7753
hakimu jackson <hakimuj@gmail.com>
2013-10-03 22:04:59 -0700

    Refactor to use class method

--- 99_bottles/bottles_redo.rb
+++ 99_bottles/bottles_redo.rb
@@ -1,31 +1,39 @@
 class Beer
 	def self.song
-		bottles_beer = (1..99).to_a.reverse
-		bottles_beer.each do |number|
-			print number
+		bottles_beer = (0..99).to_a.reverse
+		bottles_beer.each do |i|
+			if i >= 2
+				puts "#{i}" + first_verse[0] + " #{i}" + first_verse[1]
+				puts second_verse[0] +  "#{i-1}" + second_verse[1]
+			elsif i == 1
+				puts "1 more bottle of beer...than no more beers"
+			else
+				puts "No more bottles of beers"
 			end
+
+		end
 	end
 
-	def self.verse_checker(number)
-		if number > 2
-				return number.to_s + first_verse[0] + number.to_s + first_verse[1] + "\n" + second_verse[0] + number.to_s + second_verse[1]
-			elsif number == 1
-				return number.to_s + one_beer_verse[0] + number.to_s + one_beer_verse[1] + "\n" + second_verse[0] + number.to_s + second_verse[1].gsub("s", "")
-			else
-				return  last_beer_verse
-			end
-	end
+	# def self.verse_checker(number)
+	# 	if number >= 2
+	# 			return number.to_s + first_verse[0] + number.to_s + first_verse[1] + "\n" + second_verse[0] + number.to_s + second_verse[1]
+	# 		elsif number == 1
+	# 			return number.to_s + one_beer_verse[0] + number.to_s + one_beer_verse[1] + "\n" + second_verse[0] + number.to_s + second_verse[1].gsub("s", "")
+	# 		else
+	# 			return  last_beer_verse
+	# 		end
+	# end
 
 	def self.first_verse
 		first_verse = [' bottles of beer on the wall ', ' bottles of beer']
 	end
 
 	def self.second_verse
-		second_verse = ['You take one down pass it around ', ' bottles of beer on the wall.']
+		second_verse = ['You take one down pass it around, ', ' bottles of beer on the wall.']
 	end
 
 	def self.one_beer_verse
-		one_beer_verse = [' bottle of beer on the wall ', ' bottle of beer ']
+		one_beer_verse = ['1 more bottle of beer on the wall ', ' bottle of beer ']
 	end
 
 	def self.last_beer_verse
